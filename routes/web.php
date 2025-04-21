a2dcaa8dfe2c45f92315cdd47a9439f0d4e4afe7
Hayitboyevich <shhayitboyevich@gmail.com>
2025-04-21 13:01:21 +0500

    sdfasdf

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->middleware('basic');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
