e55a8cfef0bfcfc2333fbb74df0c117618e88b59
Hayitboyevich <shhayitboyevich@gmail.com>
2025-05-02 12:33:45 +0500

    sdfgsdfgsdfg

--- modules/Apartment/Http/Resources/CompanyResource.php
+++ modules/Apartment/Http/Resources/CompanyResource.php
@@ -12,8 +12,7 @@ class CompanyResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'name' => $this->name,
-            'description' => $this->description,
+            'name' => $this->company_name,
         ];
     }
 }
