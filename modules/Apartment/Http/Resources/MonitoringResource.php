36cfa7f6e9b83c528bae66cbc3c29771938da196
Hayitboyevich <shhayitboyevich@gmail.com>
2025-05-07 15:49:21 +0500

    bug fix

--- modules/Apartment/Http/Resources/MonitoringResource.php
+++ modules/Apartment/Http/Resources/MonitoringResource.php
@@ -15,7 +15,44 @@ class MonitoringResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id
+            'id' => $this->id,
+            'status' => $this->monitoring_status_id,
+            'user' => $this->user ? [
+                'id' => $this->user_id,
+                'full_name' => $this->user->full_name
+            ] : null,
+            'role' => $this->role ? [
+                'id' => $this->role_id,
+                'name' => $this->role->name
+            ] : null,
+            'monitoring_type' => $this->type ? [
+                'id' => $this->monitoring_type_id,
+                'name' => $this->type->name
+            ] : null,
+            'monitoring_base' => $this->base ? [
+                'id' => $this->monitoring_base_id,
+                'name' => $this->base->name
+            ] : null,
+            'company' => $this->company ? [
+                'id' => $this->company_id,
+                'name' => $this->company->name
+            ] : null,
+            'apartment' => $this->apartment ? [
+                'id' => $this->apartment_id,
+                'name' => $this->home_name
+            ] : null,
+            'region' => $this->region ? [
+                'id' => $this->region_id,
+                'name' => $this->region->name_uz
+            ] : null,
+            'district' => $this->district ? [
+                'id' => $this->district_id,
+                'name' => $this->district->name_uz
+            ] : null,
+            'address_commit' => $this->address_commit,
+            'lat' => $this->lat,
+            'long' => $this->long,
+            'created_at' => $this->created_at,
         ];
     }
 }
--- modules/Apartment/Models/Monitoring.php
+++ modules/Apartment/Models/Monitoring.php
@@ -2,9 +2,14 @@
 
 namespace Modules\Apartment\Models;
 
+use App\Models\District;
 use App\Models\Document;
 use App\Models\Image;
+use App\Models\Region;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Modules\Water\Enums\ProtocolStatusEnum;
 
@@ -23,5 +28,43 @@ class Monitoring extends Model
     public function images(): MorphMany
     {
         return $this->morphMany(Image::class, 'imageable');
+    }
+
+    public function type(): BelongsTo
+    {
+       return $this->belongsTo(MonitoringType::class, 'monitoring_type_id');
+    }
+
+    public function base(): BelongsTo
+    {
+        return $this->belongsTo(MonitoringBase::class, 'monitoring_base_id');
+    }
+
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class, 'role_id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class, 'company_id', 'company_id');
+    }
+
+    public function apartment(): BelongsTo
+    {
+        return $this->belongsTo(Apartment::class, 'apartment_id');
+    }
+    public function region(): BelongsTo
+    {
+        return $this->belongsTo(Region::class, 'region_id');
+    }
+
+    public function district(): BelongsTo
+    {
+        return $this->belongsTo(District::class, 'district_id');
     }
 }
