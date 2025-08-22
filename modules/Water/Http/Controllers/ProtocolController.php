18ce04b263bc72f58b38fc6d05433ccc46fcc6e9
Hayitboyevich <shhayitboyevich@gmail.com>
2025-08-22 11:27:11 +0500

    buf dix

--- modules/Water/Http/Controllers/ProtocolController.php
+++ modules/Water/Http/Controllers/ProtocolController.php
@@ -259,11 +259,11 @@ class ProtocolController extends BaseController
                     'name' => $region->name_uz,
                     'inspector_count' => $userCounts->get($regionId, 0),
                     'all_protocols' => $regionProtocols->sum('count'),
-                    'defect_count' => $regionProtocols->whereNotIn('protocol_status_id', [ProtocolStatusEnum::ENTER_RESULT,ProtocolStatusEnum::NOT_DEFECT, ProtocolStatusEnum::CONFIRM_NOT_DEFECT, ProtocolStatusEnum::REJECTED])->sum('count'),
-                    'remedy_count' => $regionProtocols->where('type', 2)->sum('count'),
-                    'confirmed_count' => $regionProtocols->where('protocol_status_id', ProtocolStatusEnum::CONFIRMED)->sum('count'),
-                    'administrative_count' => $regionProtocols->where('protocol_status_id', ProtocolStatusEnum::ADMINISTRATIVE)->sum('count'),
-                    'hmqo_count' => $regionProtocols->where('protocol_status_id', ProtocolStatusEnum::HMQO)->sum('count'),
+                    'defect_count' => $regionProtocols->whereNotIn('protocol_status_id', [ProtocolStatusEnum::ENTER_RESULT->value,ProtocolStatusEnum::NOT_DEFECT->value, ProtocolStatusEnum::CONFIRM_NOT_DEFECT->value, ProtocolStatusEnum::REJECTED->value])->sum('count'),
+                    'remedy_count' => $regionProtocols->where('category', 2)->sum('count'),
+                    'confirmed_count' => $regionProtocols->where('protocol_status_id', ProtocolStatusEnum::CONFIRMED->value)->sum('count'),
+                    'administrative_count' => $regionProtocols->where('protocol_status_id', ProtocolStatusEnum::ADMINISTRATIVE->value)->sum('count'),
+                    'hmqo_count' => $regionProtocols->where('protocol_status_id', ProtocolStatusEnum::HMQO->value)->sum('count'),
 
                 ];
             });
