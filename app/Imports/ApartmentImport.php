e140318e13f6d4f8eb4165654b87532d69939ec2
Hayitboyevich <shhayitboyevich@gmail.com>
2026-07-15 19:17:30 +0500

    asdfasdf

--- app/Imports/ApartmentImport.php
+++ app/Imports/ApartmentImport.php
@@ -7,20 +7,33 @@ use App\Models\Region;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Modules\Apartment\Models\Apartment;
 
-class ApartmentImport  implements WithHeadingRow, ToModel
+class ApartmentImport  implements WithHeadingRow, ToCollection, WithChunkReading
 {
-    public function model(array $row)
+    public function collection(Collection $rows)
     {
-        return new Apartment([
-            'company_id' => $row['company_id'],
-            'street_name' => $row['street_name'],
-            'street_id' => $row['street_id'],
-            'home_id' => $row['home_id'],
-            'home_name' => $row['home_name'],
+        $data = [];
 
-        ]);
+        foreach ($rows as $row) {
+            $data[] = [
+                'company_id' => $row['company_id'],
+                'street_name' => $row['street_name'],
+                'street_id' => $row['street_id'],
+                'home_id' => $row['home_id'],
+                'home_name' => $row['home_name'],
+                'created_at' => now(),
+                'updated_at' => now(),
+            ];
+        }
+
+        Apartment::insert($data);
+    }
+
+    public function chunkSize(): int
+    {
+        return 1000;
     }
 }
