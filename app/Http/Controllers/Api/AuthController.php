0a26540ce79340fb61569841c4567a4ece0fe4b2
jackBro0 <Jackaka0@23>
2026-08-06 18:21:55 +0500

    minor

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -68,7 +68,7 @@ class AuthController extends BaseController
         if (Auth::attempt($credentials)) {
             $user = Auth::user();
             if ($user->user_status_id != UserStatusEnum::ACTIVE->value)
-                return $this->sendError(error: 'Access denied!User is not active.', code: 403);
+                return $this->sendError(error: 'Access denied!User is not active.', code: 422);
             $token = JWTAuth::claims(['role_id' => $request->role_id])->fromUser($user);
             $role = Role::query()->find($request->role_id);
             $success['token'] = $token;
