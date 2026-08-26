8fd1822e0a64a21ec012493df55bf7d30e0dc0c9
jackBro0 <Jackaka0@23>
2026-08-26 17:26:21 +0500

    minor

--- modules/Apartment/routes/api.php
+++ modules/Apartment/routes/api.php
@@ -108,14 +108,9 @@ Route::group(['middleware' => ['auth:api', 'check-role']], function () {
         Route::get('/fines', 'fines');
     });
 
-//    Route::group(['prefix' => 'hidden-economy/apartments'], function () {
-//        Route::get('/export', [ApartmentHiddenEconomyController::class, 'export']);apartment-statictic
-//        Route::get('/{id?}', [InformationController::class, 'apartmentHiddenEconomy']);
-//        Route::post('attach-inspector', [ApartmentHiddenEconomyController::class, 'attachInspector']);
-//    });
-});
-Route::group(['prefix' => 'hidden-economy/apartments'], function () {
-    Route::get('/export', [ApartmentHiddenEconomyController::class, 'export']);
-    Route::get('/{id?}', [InformationController::class, 'apartmentHiddenEconomy']);
-    Route::post('attach-inspector', [ApartmentHiddenEconomyController::class, 'attachInspector']);
+    Route::group(['prefix' => 'hidden-economy/apartments'], function () {
+        Route::get('/export', [ApartmentHiddenEconomyController::class, 'export']);
+        Route::get('/{id?}', [InformationController::class, 'apartmentHiddenEconomy']);
+        Route::post('attach-inspector', [ApartmentHiddenEconomyController::class, 'attachInspector']);
+    });
 });
