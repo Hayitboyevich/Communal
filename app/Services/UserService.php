3ea3c34dbd5f5f48e935a18aa4339d9a4aff3d2b
jackBro0 <Jackaka0@23>
2026-09-21 15:58:03 +0500

    minor

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -53,31 +53,32 @@ class UserService
 
             $user = User::query()->where('pin', $request['pin'])->first();
             $comment = 'Foydalanuvchi qo\'shildi';
+            $eimzoSign = $this->eimzoService->signTimestamp($request['pkcs7']);
+            if (!in_array(Auth::user()?->pin, Arr::wrap(Arr::get($eimzoSign, 'pin', [])))) {
+                return response()->json(['message' => 'Elektron kalit egasi va foydalanuvchi PINFLi mos emas!'], 404);
+            }
             if (!$user) {
-                $user = $this->repository->create($request->except(['role_id', 'image', 'images', 'docs']));
+                $user = $this->repository->create($request->except(['role_id', 'image', 'images', 'docs', 'pkcs7']));
+                $userHistoryId = $this->createUserActionHistory(user_id: $user->id, comment: $comment, date: now(), status: 1, type: UserActionHistory::TYPE_CREATE, additional_info: $eimzoSign['pkcs7b64']);
+                $userHistory = UserActionHistory::query()->find($userHistoryId);
                 if ($request->hasFile('image')) {
-                    $path = $this->fileService->uploadImage($request->file('image'), 'user-history/images');
+                    $path = $this->fileService->uploadImage($request->file('image'), 'user/images');
                     $user->update(['image' => $path]);
                 }
 
                 if ($request->images) {
                     $paths = array_map(fn($file) => $this->fileService->uploadImage($file, 'user-history/images'), $request->images);
-                    $user->images()->createMany(array_map(fn($path) => ['url' => $path], $paths));
+                    $userHistory->images()->createMany(array_map(fn($path) => ['url' => $path], $paths));
                 }
                 if ($request->docs) {
                     $paths = array_map(fn($file) => $this->fileService->uploadImage($file, 'user-history/files'), $request->docs);
-                    $user->documents()->createMany(array_map(fn($path) => ['url' => $path], $paths));
+                    $userHistory->documents()->createMany(array_map(fn($path) => ['url' => $path], $paths));
                 }
             }
 
             foreach ($request->role_id as $role) {
                 $user->roles()->syncWithoutDetaching([$role]);
             }
-            $eimzoSign = $this->eimzoService->signTimestamp($request['pkcs7']);
-            if (!in_array(Auth::user()?->pin, Arr::wrap(Arr::get($eimzoSign, 'pin', [])))) {
-                return response()->json(['message' => 'Elektron kalit egasi va foydalanuvchi PINFLi mos emas!'], 404);
-            }
-            $this->createUserActionHistory(user_id: $user->id, comment: $comment, date: now(), status: 1, type: UserActionHistory::TYPE_CREATE, additional_info: $eimzoSign['pkcs7b64']);
             DB::commit();
             return $user;
         } catch (\Exception $exception) {
@@ -90,11 +91,15 @@ class UserService
     {
         DB::beginTransaction();
         try {
-
-            $user = $this->repository->update($id, $request->except(['role_id', 'image', 'images', 'docs']));
-
-            $user->roles()->syncWithoutDetaching($request->role_id);
             $comment = 'Foydalanuvchi o\'zgartirildi';
+            $eimzoSign = $this->eimzoService->signTimestamp($request['pkcs7']);
+            if (!in_array(Auth::user()?->pin, Arr::wrap(Arr::get($eimzoSign, 'pin', [])))) {
+                return response()->json(['message' => 'Elektron kalit egasi va foydalanuvchi PINFLi mos emas!'], 404);
+            }
+            $user = $this->repository->update($id, $request->except(['role_id', 'image', 'images', 'docs']));
+            $userHistoryId = $this->createUserActionHistory(user_id: $user->id, comment: $comment, date: now(), status: (int)$request['$request'], type: UserActionHistory::TYPE_UPDATE, additional_info: $eimzoSign['pkcs7b64']);
+            $userHistory = UserActionHistory::query()->find($userHistoryId);
+            $user->roles()->syncWithoutDetaching($request->role_id);
 
             if (!empty($request->image)) {
                 $path = $this->fileService->uploadImage($request->image, 'user/images');
@@ -103,18 +108,13 @@ class UserService
 
             if (!empty($request->images)) {
                 $paths = array_map(fn($file) => $this->fileService->uploadImage($file, 'user/images'), $request->images);
-                $user->images()->createMany(array_map(fn($path) => ['url' => $path], $paths));
+                $userHistory->images()->createMany(array_map(fn($path) => ['url' => $path], $paths));
             }
 
             if (!empty($request->docs)) {
                 $paths = array_map(fn($file) => $this->fileService->uploadImage($file, 'user/files'), $request->docs);
-                $user->documents()->createMany(array_map(fn($path) => ['url' => $path], $paths));
+                $userHistory->documents()->createMany(array_map(fn($path) => ['url' => $path], $paths));
             }
-            $eimzoSign = $this->eimzoService->signTimestamp($request['pkcs7']);
-            if (!in_array(Auth::user()?->pin, Arr::wrap(Arr::get($eimzoSign, 'pin', [])))) {
-                return response()->json(['message' => 'Elektron kalit egasi va foydalanuvchi PINFLi mos emas!'], 404);
-            }
-            $this->createUserActionHistory(user_id: $user->id, comment: $comment, date: now(), status: (int)$request['$request'], type: UserActionHistory::TYPE_UPDATE, additional_info: $eimzoSign['pkcs7b64']);
 
             DB::commit();
             return $user;
@@ -196,15 +196,14 @@ class UserService
                     'created_at' => $item->created_at,
                 ];
             })->sortByDesc('created_at')->values();
-            dd($result);
         } catch (\Exception $exception) {
             throw $exception;
         }
     }
 
-    private function createUserActionHistory(int $user_id, ?string $comment, ?string $date, int $status, int $type, mixed $additional_info = null): void
+    private function createUserActionHistory(int $user_id, ?string $comment, ?string $date, int $status, int $type, mixed $additional_info = null)
     {
-        $this->historyService->createHistory(guid: $user_id, status: $status, type: $type, date: $date, comment: $comment, additionalInfo: $additional_info);
+        return $this->historyService->createHistory(guid: $user_id, status: $status, type: $type, date: $date, comment: $comment, additionalInfo: $additional_info);
     }
 
 }
