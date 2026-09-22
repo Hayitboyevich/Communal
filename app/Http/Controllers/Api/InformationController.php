bfe08e28a868337b43a2bd58933f833378add75e
jackBro0 <Jackaka0@23>
2026-09-22 18:33:43 +0500

    minor

--- app/Http/Controllers/Api/InformationController.php
+++ app/Http/Controllers/Api/InformationController.php
@@ -289,7 +289,6 @@ class InformationController extends BaseController
         $result = Http::withHeaders([
             'Authorization' => 'Basic SXVnQ2h4XzFabkxsQWhkMEp4OWVtTjZqV3AwYToxUzlrWGxLQzBhWnd3bHNzb28xSzJmM1NRN3dh'
         ])
-            ->timeout(15)
             ->post("https://iskm.egov.uz:9444/oauth2/token?grant_type=password&username=qv-user&password=8F5zl2w68GU1itlyGF0w");
 
         $res = json_decode($result->body());
